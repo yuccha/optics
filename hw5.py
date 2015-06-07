80ab14d97ba5199e8cd0fc80e1cede914f6dc5b6
yuccha <erwin.yuchia.chen@gmail.com>
2015-06-08 00:31:51 +0800

    impulse response
    1. added the impulse response to sample_hw5.py
    2. correct the error of Intensity and E field in hw5.py
    3. change the boundary condition of the diagram.

--- hw5.py
+++ hw5.py
@@ -6,7 +6,7 @@ lamda = 500E-9
 #d = 50E-6
 scn = 1E-6
 tms = 500
-rds_tm = 50
+rds_tm = 25
 rds = scn*rds_tm
 
 side = np.linspace(-scn*tms,scn*tms,tms)
@@ -18,7 +18,7 @@ E_ca = x**2+y**2 < rds**2
 D1 = np.fft.fft2(E_ca)
 D1 = np.fft.fftshift(D1)
 
-I_ca = np.abs(D1)/((lamda*R)**2)
+I_ca = (np.abs(D1)/(lamda*R))**2
 
 f1 = plt.figure(1)
 plt.imshow(I_ca)
@@ -33,7 +33,7 @@ E_ca_sm = x**2+y**2 < (rds/2)**2
 D1 = np.fft.fft2(E_ca_sm)
 D1 = np.fft.fftshift(D1)
 
-I_ca_sm = np.abs(D1)/((lamda*R)**2)
+I_ca_sm = (np.abs(D1)/(lamda*R))**2
 
 f3 = plt.figure(3)
 plt.imshow(I_ca_sm)
@@ -52,7 +52,7 @@ E_zh = M2 ^ M1 | M3
 D1 = np.fft.fft2(E_zh)
 D1 = np.fft.fftshift(D1)
 
-I_zh = np.abs(D1)/((lamda*R)**2)
+I_zh = (np.abs(D1)/(lamda*R))**2
 
 f5 = plt.figure(5)
 plt.imshow(I_zh)
--- sample_hw5.py
+++ sample_hw5.py
@@ -1,53 +1,86 @@
 import pylab as plt
 import numpy as np
+import copy
 
 R = 1
 lamda = 500E-9
-d = 50E-6
-side = np.linspace(-0.01*np.pi, 0.01*np.pi, 200)
-x, y = np.meshgrid(side, side)
+scn = 1E-6
+tms = 400
+rds_tm = 25
+#rds = scn*rds_tm
+rds = 25E-6
 
-side_2 = np.linspace(-5*d, 5*d, 200)
-#side_2 = np.linspace(0.1,0.1,200)
-x_0, y_0 = np.meshgrid(side_2, side_2)
+#----- Diffraction diagram of circular aperture (by impulse response method.) -----#
+side = np.linspace(-0.05*np.pi, 0.05*np.pi, 200)
+x,y = np.meshgrid(side, side)
+x0,y0 = np.meshgrid(np.linspace(0,0,200),np.linspace(0,0,200))	# used to initialize E_ca
+E_ca = x0+y0*1j
 
-E_field = x_0**2+y_0**2 < (d/2)**2
-E_field.astype(float)
+for xid in np.arange(-30*scn, 30*scn, scn):
+	for yid in np.arange(-30*scn, 30*scn, scn):
+		if np.sqrt(xid**2+yid**2) < rds:
+			E_ca += np.exp(1j*((x-xid)**2+(y-yid)**2)/(R*lamda))
 
+I_ca = (np.abs(E_ca)/(lamda*R))**2
 
+I_ca2 = copy.deepcopy(I_ca)
 
-D1 = np.fft.fft2(E_field)
-
-D2=D1
-D2 = np.fft.fftshift(D1)
-#D2=E_field
-abs_image = np.abs(D2)
-#plt.imshow(abs_image)
-#plt.show()
-
-
-#intensity = abs(E_field)**2
-
-#print intensity[100]
+r_crit = 1.22*lamda*R/(2*rds)
+E_c = (x**2+y**2 < 1.1*r_crit**2) & (x**2+y**2 > 0.8*r_crit**2)
 
 f1 = plt.figure(1)
-plt.pcolormesh(x, y, abs_image)
-f1.show()
+plt.imshow(I_ca)
+#plt.pcolormesh(x,y,I_ca)
 
-#f1 = plt.figure(1)
-#plt.pcolormesh(x, y, intensity)
-#f1.show()
+#--- fig 2 shows the diffraction diagram with a reddish-brown ring, which denotes the Rayleigh criteria in theory ---# 
+f2 = plt.figure(2)
+plt.imshow(I_ca2)
+#plt.pcolormesh(x,y,I_ca2)
 
-#f2 = plt.figure(2)
-#plt.gray()
-#plt.pcolormesh(x, y, intensity)
-#f2.show()
+I_c = (np.abs(E_c)/(lamda*R))**2
+I_ca2 += (I_c*10E19)	# multiply with the highest peak so that we can make this ring standout.
 
-#f3 = plt.figure(3)
-#plt.jet()
-#plt.pcolormesh(x, y, np.sqrt(intensity))
-#f3.show()
+#----- Diffraction matrix of circular aperture, with radius = 50 um (by Fourier Transform method.) -----#
+side = np.linspace(-scn*tms,scn*tms,tms/2)
+x1,y1 = np.meshgrid(side, side)
+E_cf = x1**2+y1**2 < rds**2
 
+D1 = np.fft.fft2(E_cf)
+D1 = np.fft.fftshift(D1)
 
+I_cf = (np.abs(D1)/(lamda*R))**2
+
+f3 = plt.figure(3)
+plt.imshow(I_cf)
+
+#----- Smaller circular aperture, radius = 25 um (by Fourier Transform) -----#
+E_cf_sm = x1**2+y1**2 < (rds/2)**2
+
+D1 = np.fft.fft2(E_cf_sm)
+D1 = np.fft.fftshift(D1)
+
+I_cf_sm = (np.abs(D1)/(lamda*R))**2
+
+f4 = plt.figure(4)
+plt.imshow(I_cf_sm)
+
+#----- Diffraction diagram of the Chinese character "Zhong" -----#
+M1 = (np.abs(0.7*x1) < rds) & (np.abs(1.4*y1) < rds)
+M2 = (np.abs(1.05*x1) < rds) & (np.abs(2.1*y1) < rds)
+M3 = (np.abs(3.5*x1) < rds) & (np.abs(0.7*y1) < rds)
+
+E_zh = M2 ^ M1 | M3
+
+D1 = np.fft.fft2(E_zh)
+D1 = np.fft.fftshift(D1)
+
+I_zh = (np.abs(D1)/(lamda*R))**2
+
+f5 = plt.figure(5)
+plt.imshow(I_zh)
+
+#--- fig 6 shows the shape of the aperture ---#
+f6 = plt.figure(6)
+plt.imshow(E_zh)
 
 plt.show()
